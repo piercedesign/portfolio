c6924d0a5a83e424cddc9994123a7d0f071882bf
piercedesign22 <piercedesign22@gmail.com>
2021-03-19 15:10:55 -0500

    Updated resume content

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,24 @@
-$(function(){$(".lazy").Lazy({effect:"fadeIn",effectTime:1e3,threshold:100})});
+/******/ (function() { // webpackBootstrap
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+// import $ from 'jquery';
+// import whatInput from 'what-input';
+//
+// window.$ = $;
+//
+// import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+$(function () {
+  $('.lazy').Lazy({
+    //scrollDirection: vertical,
+    effect: 'fadeIn',
+    effectTime: 1000,
+    threshold: 100
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
